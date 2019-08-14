6cff2c070ffdaa138cdb2820c670b654c4f536a7
zcodes <zcodes@163.com>
2019-08-14 14:48:46 +0800

    update custom theme

--- custom/themes/zcodes/zcodes.theme.bash
+++ custom/themes/zcodes/zcodes.theme.bash
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 SCM_THEME_PROMPT_DIRTY=" ${red}✗"
-SCM_THEME_PROMPT_CLEAN=" ${bold_green}✓"
+SCM_THEME_PROMPT_CLEAN=" ${green}✓"
 SCM_THEME_PROMPT_PREFIX=" |"
 SCM_THEME_PROMPT_SUFFIX="${green}|"
 
 GIT_THEME_PROMPT_DIRTY=" ${red}✗"
-GIT_THEME_PROMPT_CLEAN=" ${bold_green}✓"
+GIT_THEME_PROMPT_CLEAN=" ${green}✓"
 GIT_THEME_PROMPT_PREFIX=" ${green}|"
 GIT_THEME_PROMPT_SUFFIX="${green}|"
 
@@ -31,12 +31,12 @@ __bobby_clock() {
 }
 
 function prompt_command() {
-    PS1="${TITLEBAR}\n${green}#$(virtualenv_prompt) $(__bobby_clock)${yellow}$(ruby_version_prompt) ${purple}\h ${reset_color}in ${green}\w\n${bold_cyan}${green}=>${reset_color} "
+    PS1="${TITLEBAR}\n${green}#$(virtualenv_prompt) $(__bobby_clock)${yellow}$(ruby_version_prompt) ${purple}\h ${reset_color}in ${green}\w\n${cyan}${green}=>${reset_color} "
 }
 
 THEME_SHOW_CLOCK_CHAR=${THEME_SHOW_CLOCK_CHAR:-"true"}
 THEME_CLOCK_CHAR_COLOR=${THEME_CLOCK_CHAR_COLOR:-"$red"}
-THEME_CLOCK_COLOR=${THEME_CLOCK_COLOR:-"$bold_cyan"}
+THEME_CLOCK_COLOR=${THEME_CLOCK_COLOR:-"$cyan"}
 THEME_CLOCK_FORMAT=${THEME_CLOCK_FORMAT:-"%Y-%m-%d %H:%M:%S"}
 
 safe_append_prompt_command prompt_command
